9d3c8391e05c10732019f4ccde62093a29ce1705
Michael V M <michael.mcconnell@uvm.edu>
2022-12-15 09:43:44 -0500

    Update parser.js

--- parser.js
+++ parser.js
@@ -20,5 +20,45 @@ catch(e){
 let value = parser.results.flat();
 
 for (let i = 0; i < value.length; i++){
-    console.log(JSON.stringify(value[i]));
+    //console.log(JSON.stringify(value[i]));
+    let formattedstr = convert(value[i]);
+    console.log(formattedstr);
 }
+
+
+//console.log(JSON.stringify(parser.results));
+
+
+function convert(arr){
+    var result = "";
+    for(var i = 0; i < arr.length; i++){
+        if(Array.isArray(arr[i])){
+            result += "(" + convert(arr[i]) + ")";
+        } else {
+            result += arr[i];
+        }
+        if(i < arr.length - 1){
+            result += ",";
+        }
+    }
+    return format(result);
+}
+
+function format(str){
+    var newstr = "";
+    for(var i = 0; i < str.length; i++){
+      if(str[i] === "," && str[i+1] === "("){
+        newstr += "(";
+        i++;
+      }
+      else if(str[i] === ")" && str[i+1] === "," && str[i+2] === "("){
+        newstr += "),(";
+        i += 2;
+      }
+      else{
+        newstr += str[i];
+      }
+    }
+    //console.log(newstr);
+    return newstr;
+  }
