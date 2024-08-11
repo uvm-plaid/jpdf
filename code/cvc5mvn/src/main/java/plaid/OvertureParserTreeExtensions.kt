0bde6321528fbf163108d5f769c2078c0f2fe1de
Steven Baldasty <sbaldasty@gmail.com>
2024-08-11 11:24:04 -0400

    parser tests

--- code/cvc5mvn/src/main/java/plaid/OvertureParserTreeExtensions.kt
+++ code/cvc5mvn/src/main/java/plaid/OvertureParserTreeExtensions.kt
@@ -1,5 +1,20 @@
 package plaid
 
 import plaid.OvertureParser.AssignmentContext
+import plaid.OvertureParser.AtpartyContext
+import plaid.OvertureParser.ProgramContext
 
-fun AssignmentContext.sourceParty() = this.atparty().party().VALUE().text
+fun AssignmentContext.destinationPartyId() =
+    dest().atparty().partyId()
+
+fun AssignmentContext.sourcePartyId() =
+    atparty().partyId()
+
+fun AtpartyContext.partyId(): String =
+    party().VALUE().text
+
+fun ProgramContext.assignmentCount() =
+    assignment().size
+
+fun ProgramContext.firstAssignment(): AssignmentContext =
+    assignment().first()
--- code/cvc5mvn/src/test/java/plaid/OvertureParserTest.kt
+++ code/cvc5mvn/src/test/java/plaid/OvertureParserTest.kt
@@ -1,10 +1,10 @@
 package plaid
 
+import junit.framework.TestCase.assertEquals
 import org.antlr.v4.runtime.ANTLRInputStream
 import org.antlr.v4.runtime.BailErrorStrategy
 import org.antlr.v4.runtime.CommonTokenStream
 import org.antlr.v4.runtime.misc.ParseCancellationException
-import org.junit.Assert
 import org.junit.Test
 import plaid.OvertureParser.ProgramContext
 
@@ -25,22 +25,42 @@ class OvertureParserTest {
      * Programs must have at least one assignment.
      */
     @Test(expected = ParseCancellationException::class)
-    fun empty() {
+    fun assignmentCount0() {
         loadProgram("")
     }
 
     /**
-     * Nonsensical assignment but similar to example in Section 2.3.
+     * Recognizes programs that have one assignment.
      */
     @Test
-    fun assignOutput() {
-        // TODO This line doesn't work when using IDENTIFIER instead of VALUE
-        //ProgramContext program = loadProgram("out @ 1 := (p[1] + p[2] + p[3]) @ 4");
-        val program = loadProgram("out @ 1 := (p[1] + p[2] + p[3]) @ 4")
-        val assignments = program.assignment()
-        Assert.assertEquals("Program should have one assignment", 1, assignments.size)
+    fun assignmentCount1() {
+        val program = loadProgram("out @ 1 := 18 @ 1")
+        assertEquals(1, program.assignmentCount())
+    }
 
-        val assignment = program.assignment().first()
-        Assert.assertEquals("4", assignment.sourceParty())
+    /**
+     * Recognizes programs that have multiple assignments.
+     */
+    @Test
+    fun assignmentCount2() {
+        val src =
+            """
+            out @ 1 := 18 @ 1
+            out @ 2 := 18 @ 2
+            """
+
+        val program = loadProgram(src)
+        assertEquals(2, program.assignmentCount())
+    }
+
+    /**
+     * Recognizes the source and destination parties in an assignment.
+     */
+    @Test
+    fun assignmentParties() {
+        val program = loadProgram("out @ 1 := 18 @ 4")
+        val assignment = program.firstAssignment()
+        assertEquals("1", assignment.destinationPartyId())
+        assertEquals("4", assignment.sourcePartyId())
     }
 }
