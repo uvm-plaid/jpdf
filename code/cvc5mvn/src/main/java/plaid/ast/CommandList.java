504f7873a53d6e476fc67a464d8a6b7cca173442
yyeonhee <yeonheeyeh@github.com>
2024-09-27 13:37:40 -0400

    Prelude AST (with updated 'CommandList')

--- code/cvc5mvn/src/main/java/plaid/ast/CommandList.java
+++ code/cvc5mvn/src/main/java/plaid/ast/CommandList.java
@@ -1,6 +1,15 @@
 package plaid.ast;
 
-public class CommandList implements PreludeCommand{
-    //private final PreludeCommand c;
+import java.util.List;
 
+public class CommandList implements PreludeCommand{
+    private final List<PreludeCommand> commands;
+
+    public CommandList(List<PreludeCommand> commands){
+        this.commands = commands;
+    }
+
+    public List<PreludeCommand> getCommands() {
+        return commands;
+    }
 }
