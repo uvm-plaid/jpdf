532f89b99c759432a4e0009b9c4335b19c58e6b0
Steven Baldasty <sbaldasty@gmail.com>
2024-10-04 10:21:19 -0400

    implement verifier tests

--- code/cvc5mvn/src/test/java/plaid/cvc/VerifierTest.java
+++ code/cvc5mvn/src/test/java/plaid/cvc/VerifierTest.java
@@ -2,22 +2,25 @@ package plaid.cvc;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class VerifierTest {
 
     /**
-     * Verifies a protocol.
+     * Verifies a simple protocol.
      */
     @Test
     public void verifyCorrectProtocol() {
-        // TODO Implement me
+        assertTrue(Verifier.verify("out@1 := 1; out@2 := 2"));
     }
 
     /**
-     * Fails to verify an incorrect protocol.
+     * Fails to verify a simple contradictory protocol.
      */
     @Test
     public void failIncorrectProtocol() {
-        // TODO Implement me
+        assertFalse(Verifier.verify("out@1 := 1; out@1 := 2"));
     }
 
 }
