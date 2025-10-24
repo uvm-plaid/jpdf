af08fb167954d0e2e3dd21782ddb5df32d72bf71
Steven Baldasty <sbaldasty@gmail.com>
2025-10-24 12:20:37 -0400

    remove unused inheritance

--- code/cvc5mvn/src/main/scala/plaid/ast/expr.scala
+++ code/cvc5mvn/src/main/scala/plaid/ast/expr.scala
@@ -1,6 +1,5 @@
 package plaid.ast
 
-// TODO Why do Str and Num extend Type?
 trait PreludeExpression extends Node
 case class AtExpr(e1: PreludeExpression, e2: PreludeExpression) extends PreludeExpression
 case class ConcatExpr(e1: PreludeExpression, e2: PreludeExpression) extends PreludeExpression
@@ -9,9 +8,9 @@ case class FieldSelectExpr(e: PreludeExpression, l: Identifier) extends PreludeE
 case class FunctionCallExpr(fname: Identifier, parameters: java.util.List[PreludeExpression]) extends PreludeExpression, ConstraintExpr
 case class LetExpr(y: Identifier, e1: PreludeExpression, e2: PreludeExpression) extends PreludeExpression
 case class MinusExpr(e: PreludeExpression) extends PreludeExpression
-case class Num(num: Int) extends PreludeExpression, Type
+case class Num(num: Int) extends PreludeExpression
 case class OTExpr(e1: PreludeExpression, i1: PreludeExpression, e2: PreludeExpression, e3: PreludeExpression) extends PreludeExpression
 case class OTFourExpr(s1: PreludeExpression, s2: PreludeExpression, i1: PreludeExpression, e1: PreludeExpression, e2: PreludeExpression, e3: PreludeExpression, e4: PreludeExpression) extends PreludeExpression
 case class PlusExpr(e1: PreludeExpression, e2: PreludeExpression) extends PreludeExpression
-case class Str(str: String) extends PreludeExpression, Type
+case class Str(str: String) extends PreludeExpression
 case class TimesExpr(e1: PreludeExpression, e2: PreludeExpression) extends PreludeExpression
